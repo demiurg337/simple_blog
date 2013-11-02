bcc8cf591303ecf75304c969ea58e5b44c4e7e90
demiurg337 <dmitro.gedz@gmail.com>
2013-11-02 19:57:48 +0200

    Add first view

---
+++ core/admin/views/add_post.php
@@ -0,0 +1,8 @@
+<form action="<?php echo $this->getUrl('post/add'); ?>" method="post">
+    <input type="text" name="caption">
+    <br>
+    <textarea name="content">
+    </textarea>
+    <br>
+    <input type="submit" value="Send"/>
+</form>
