c1bbdd3075089bf156bccf521397121c2b6a8297
demiurg337 <dmitro.gedz@gmail.com>
2013-11-03 16:42:14 +0200

    Some changes in post controller for work with post model

--- core/admin/controllers/PostController.php
+++ core/admin/controllers/PostController.php
@@ -1,14 +1,41 @@
 <?php
-
+require_once __DIR__.'/../models/PostModel.php';
 class PostController 
 {
     public function index()
     {
-        echo 'posts.list';
+        $model = new PostModel();
+        var_dump($model->getListPosts());
     }
 
     public function add()
     {
-        echo 'zzzzzzzzzzzzzzzzzzzzzzzzzzzz';
-    } 
+        $res = $this->render('add_post');
+        
+        if (isset($_POST['content']) && isset($_POST['caption'])) {
+            $model = new PostModel();
+            $model->savePost($_POST['title'], $_POST['content']);
+        }
+       
+       echo $res; 
+    }
+
+    public function render($name, array $params = array())
+    {
+        ob_start();
+        ob_implicit_flush(false);
+        require  ($this->getViewBasePath().'/'.$name.'.php');
+        return ob_get_clean();
+    }
+
+    protected function getViewBasePath()
+    {
+        return BASE_PATH.'/core/admin/views';
+    }
+
+    public function getUrl($route)
+    {
+        return '/index.php/posts/add';
+    }
+
 }
