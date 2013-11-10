214e4e327f81b0991576701a4ee479d4f855bf7e
demiurg337 <dmitro.gedz@gmail.com>
2013-11-10 16:42:38 +0200

    Add tinymce redactor to form add post

--- core/admin/views/add_post.php
+++ core/admin/views/add_post.php
@@ -6,3 +6,17 @@
     <br>
     <input type="submit" value="Send"/>
 </form>
+
+<script type="text/javascript" src="/js/tinymce/tinymce.min.js"></script>
+<script type="text/javascript">
+
+tinymce.init({
+    selector: "textarea",
+    plugins: [
+        "advlist autolink lists link image charmap print preview anchor",
+        "searchreplace visualblocks code fullscreen",
+        "insertdatetime media table contextmenu paste"
+    ],
+    toolbar: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
+});
+</script>
