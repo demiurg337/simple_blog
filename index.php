13f43c3595c743edb7532a96874fdca47d48d632
demiurg337 <dmitro.gedz@gmail.com>
2013-11-24 12:02:30 +0200

    Create new router

--- index.php
+++ index.php
@@ -1,14 +1,42 @@
 <?php 
-//ini_set('display_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors',1);
+error_reporting(E_ALL);
 
 header('Content-Type: text/html; charset=utf-8');
-define(BASE_PATH, __DIR__);
+define('BASE_PATH', __DIR__);
 $route = $_SERVER['REQUEST_URI'];
 $route = str_replace('/index.php/','', $route);
-$parts = explode('/', $route);
-$parts[0] = ucfirst($parts[0]);
 
+
+$parts = explode('/', $route);
+$controllerPath = '';
+//if only two parts then site route
+if (sizeof($parts) === 2) {
+    //$controllerPath = BASE_PATH.'/core/admin/'.$parts[0].'Controller';
+}
+//if three routes then is route (like /admin/post/index)
+elseif (sizeof($parts) ===3) {
+    $parts[1] = ucfirst($parts[1]);
+    $controllerName = $parts[1].'Controller';
+    $actionName = $parts[2];
+    $controllerPath = BASE_PATH.'/core/admin/controllers/'.$controllerName.'.php';
+}
+
+
+if (file_exists($controllerPath)) {
+    require_once $controllerPath;
+    
+    if (!method_exists($controllerName, $actionName)) {
+        //-------------------------
+    }
+
+    $controller = new $controllerName();
+    $controller->$actionName();
+    
+    var_dump($controller);
+}
+
+die;
 require_once __DIR__.'/core/admin/controllers/PostController.php';
 $c = new PostController;
 
